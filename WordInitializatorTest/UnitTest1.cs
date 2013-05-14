64194bb08f0b78c6b36f9f77e089fb4ee7637f59
Anna Stoqnowa <naturalna89@gmail.com>
2013-05-14 21:36:16 +0300

    UnitTest1.cs SecondTry to commit

---
+++ WordInitializatorTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Hangman;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordInitializatorTest
+{
+    [TestClass]
+    public class GuessRandererTests
+    {
+        [TestMethod]
+        public void GuessRandererTest_OrderedLettersMaskMustBeCorrect()
+        {
+            string word = "SomeWord";
+            GuessRanderer randerer = new GuessRanderer();
+            randerer.GameInisialization(word);
+            Assert.IsTrue(randerer.OrderedLettersMask.Length == word.Length);  
+        }
+
+        [TestMethod]
+        public void InitializationAfterTheGuessTest()
+        {
+            string word = "SomeWord";
+            GuessRanderer randerer = new GuessRanderer();
+            randerer.GameInisialization(word);
+            randerer.InitializationAfterTheGuess(word, 'a');
+            Assert.IsTrue(randerer.NotGuessedCharsCounter == 1);    
+        }
+
+        [TestMethod]
+        public void InitializationAfterTheGuessTest_TwoGestLettersAtTheSameTime()
+        {
+            string word = "SomeWord";
+            GuessRanderer randerer = new GuessRanderer();
+            randerer.GameInisialization(word);
+            randerer.InitializationAfterTheGuess(word, 'o');
+            Assert.IsTrue(randerer.GuessedCharsCounter == 2);
+        }
+
+        [TestMethod]
+        public void RevealTheNextLetterByHelpTest_useHelpTwice()
+        {
+            string word = "test";
+            GuessRanderer randerer = new GuessRanderer();
+            randerer.GameInisialization(word);
+            randerer.RevealTheNextLetterByHelp(word);
+            randerer.RevealTheNextLetterByHelp(word);
+            Assert.AreEqual(true, randerer.IsPlayerUsedHelp);
+        }
+    }
+}
