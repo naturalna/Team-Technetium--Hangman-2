d966837c70b272a02ea7b45920aabea23dc8e0a8
Marieta Bozhidarova Vodenicharska - Georgieva <marietambvg@gmail.com>
2013-05-15 14:27:05 +0300

    Internal documentation for CommandExecuter class

--- Hangman-2/CommandExecuter.cs
+++ Hangman-2/CommandExecuter.cs
@@ -1,17 +1,29 @@
-﻿using System;
+﻿//----------------------------------------------------------------------------------
+// <copyright file="CommandExecuter.cs" company="Teleric Academy Technetium Team">
+// Teleric Academy
+// </copyright>
+//----------------------------------------------------------------------------------
 
 namespace Hangman
 {
+    using System;
+
+    /// <summary>
+    /// Executes commands given by player
+    /// </summary>
     public static class CommandExecuter
     {
-        public static void Start() //used to be Restart
+        /// <summary>
+        /// Starts the game
+        /// </summary>
+        public static void Start() // used to be Restart
         {
             Console.WriteLine();
             WordSelector wordSelector = new WordSelector();
             string word = wordSelector.SelectRandomWord();
             GuessHandler randerer = new GuessHandler();
             randerer.PlayRound(word);
-            WordGuesser wordGuesser = new WordGuesser(); //used to be wg
+            WordGuesser wordGuesser = new WordGuesser(); // used to be wg
             WordGuesser.Word = word;
 
             while (randerer.GuessedCharsCounter < word.Length && WordGuesser.HasExited == false)
@@ -20,11 +32,17 @@ namespace Hangman
             }
         }
 
+        /// <summary>
+        /// Print top results
+        /// </summary>
         public static void Top()
         {
             PlayersScore.PrintTopResults();
         }
 
+        /// <summary>
+        /// Exit the game
+        /// </summary>
         public static void Exit()
         {
             Console.WriteLine("Good bye!");
