4de8996b34008f649cadab66748a4fa82bc8f077
Marieta Bozhidarova Vodenicharska - Georgieva <marietambvg@gmail.com>
2013-05-15 15:30:12 +0300

    Internal Documentation for WordGuesser ad WordSelector Classes

--- Hangman-2/WordGuesser.cs
+++ Hangman-2/WordGuesser.cs
@@ -1,47 +1,68 @@
-﻿using System;
-using System.Linq;
+﻿//----------------------------------------------------------------------------------
+// <copyright file="WordGuesser.cs" company="Teleric Academy Technetium Team">
+// Teleric Academy
+// </copyright>
+//---------------------------------------------------------------------------------
 
 namespace Hangman
 {
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Manages the user input
+    /// </summary>
     public class WordGuesser
     {
+        /// <summary>
+        /// Word to be guessed by player
+        /// </summary>
         private static string word;
+
+        /// <summary>
+        /// Keeps track whether the user wants to exit the game
+        /// </summary>
         private static bool hasExited;
 
-        public static bool HasExited //used to be IsExited
+        /// <summary>
+        /// Gets or sets a value indicating whether the player wants to exit
+        /// </summary>
+        public static bool HasExited // used to be IsExited
         {
             get
             {
                 return hasExited;
             }
+
             set
             {
                 hasExited = value;
             }
         }
 
+        /// <summary>
+        /// Gets or sets a word to be guessed by player
+        /// </summary>
         public static string Word
         {
             get
             {
                 return word;
             }
+
             set
             {
                 word = value;
             }
         }
-
-        private string ReadUserInput()
+        
+        /// <summary>
+        /// Handle player input
+        /// </summary>
+        /// <param name="guessHandler">Data input by player</param>
+        public void HandleUserInput(GuessHandler guessHandler) // used to be GuessLetter
         {
-            Console.WriteLine("Enter your guess: ");
-            string input = Console.ReadLine();
-            return input;
-        }
-
-        public void HandleUserInput(GuessHandler guessHandler) //used to be GuessLetter
-        {
-            string supposedCharOrCommand = ReadUserInput();
+            string supposedCharOrCommand = this.ReadUserInput();
 
             if (supposedCharOrCommand.Length == 1)
             {
@@ -64,7 +85,17 @@ namespace Hangman
             {
                 CommandExecuter.Top();
             }
+        }
 
+        /// <summary>
+        /// Read user input
+        /// </summary>
+        /// <returns>String value - Player input</returns>
+        private string ReadUserInput()
+        {
+            Console.WriteLine("Enter your guess: ");
+            string input = Console.ReadLine();
+            return input;
         }
     }
 }
--- Hangman-2/WordSelector.cs
+++ Hangman-2/WordSelector.cs
@@ -1,30 +1,50 @@
-﻿using System;
-using System.Linq;
+﻿//----------------------------------------------------------------------------------
+// <copyright file="WordSelector.cs" company="Teleric Academy Technetium Team">
+// Teleric Academy
+// </copyright>
+//---------------------------------------------------------------------------------
 
 namespace Hangman
 {
-    class WordSelector
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Selects random word to be guessed by player
+    /// </summary>
+    public class WordSelector
     {
-        private static readonly string[] words =
+        /// <summary>
+        /// Keeps string array with the possible words to be guessed by player
+        /// </summary>
+        private static readonly string[] Words =
         {
             "computer", "programmer", "software", "debugger", "compiler",
             "developer", "algorithm", "array", "method", "variable"
         };
 
+        /// <summary>
+        /// Select random word from a predefined array of string values
+        /// </summary>
+        /// <returns>A random word</returns>
+        public string SelectRandomWord()
+        {
+            // including 0, exluding word.Length
+            int randomPosition = RandomNumber(0, Words.Length); // used to be randomPositionOfTheWordToBeSelected
+            string randomlySelectedWord = Words.ElementAt(randomPosition);
+            return randomlySelectedWord;
+        }
+
+        /// <summary>
+        /// Calculate random number between min and max values
+        /// </summary>
+        /// <param name="min">Lower border of interval from which the number will be selected</param>
+        /// <param name="max">Upper border of interval from which the number will be selected</param>
+        /// <returns>Random integer number</returns>
         private static int RandomNumber(int min, int max)
         {
             Random random = new Random();
             return random.Next(min, max);
         }
-
-        public string SelectRandomWord()
-        {
-            //including 0, exluding word.Length
-            int randomPosition = RandomNumber(0, words.Length);//used to be randomPositionOfTheWordToBeSelected
-            string randomlySelectedWord = words.ElementAt(randomPosition);
-            return randomlySelectedWord;
-        }
-
-        
     }
 }
